355cfe98c878bad5f7e0b1db4202be70b257f8c6
jack <997228906@qq.com>
2019-12-02 11:09:23 +0800

    调用FirstClass中的test方法

--- git-test-project/src/com/javahuang/git/FirstClass.java
+++ git-test-project/src/com/javahuang/git/FirstClass.java
@@ -9,9 +9,10 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         //hello test
+        FirstClass.test();
     }
 
-    public void test(){
+    public static void test(){
         int i = 0;
         i += 10;
         System.out.println(i);
