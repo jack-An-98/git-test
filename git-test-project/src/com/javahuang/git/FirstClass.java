5728755ea85117a13e5b14192b1ed747a85f1110
jack <997228906@qq.com>
2019-12-02 10:51:51 +0800

    向idea中添加一个git工程

---
+++ git-test-project/src/com/javahuang/git/FirstClass.java
@@ -0,0 +1,19 @@
+package com.javahuang.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        //hello test
+    }
+
+    public void test(){
+        int i = 0;
+        i += 10;
+        System.out.println(i);
+    }
+}
